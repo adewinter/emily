70699d9bad950854c89590ea3dd65be3e633e43c
Anton de Winter <adewinter@dimagi.com>
2012-05-29 11:09:45 -0300

    More code cleanup

--- emily/urls.py
+++ emily/urls.py
@@ -1,8 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.conf import settings
-
-# Uncomment the next two lines to enable the admin:
 from django.contrib import admin
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -16,7 +15,6 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 )
-
 
 if settings.DEBUG:
     urlpatterns += patterns('',
